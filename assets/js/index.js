592b3e8d821c08e94bd364b38e6f9d1f1d67c794
rogerio bueno <rogerio.bueno@globalsys.com.br>
2022-12-21 14:11:15 -0300

    feat:header section-two and section-three

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+let to =new Date('jan 1 2023 00:00:00');
+		function update(){
+			let from =new Date();
+				diff=to-from;
+				if(diff>0){
+				let days=setTwoDigit(Math.floor(diff/1000/60/60/24)),
+				hours=setTwoDigit(Math.floor(diff/1000/60/60)%24),
+				min=setTwoDigit(Math.floor(diff/1000/60)%60),
+				sec=setTwoDigit(Math.floor(diff/1000)%60)
+				document.querySelector('#days').innerText=days
+				document.querySelector('#hours').innerText=hours
+				document.querySelector('#min').innerText=min
+				document.querySelector('#sec').innerText=sec
+			}else{
+				clearInterval(interval);
+				document.querySelector('body').classList.add('alert')
+			}
+
+		}
+		
+		let interval=setInterval(update,1000)
+
+		function setTwoDigit(argument) {
+			return argument>9?argument:'0'+argument;
+		}
